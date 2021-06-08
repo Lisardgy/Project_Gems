1de6002036d93f10633fc2bbff400e78e07f46c9
wattana <endnapadad25@gmail.com>
2021-06-08 22:22:56 +0700

    add_buttonConfirm

---
+++ src/pages/form.vue
@@ -0,0 +1,32 @@
+ <template>
+  <q-layout view="lHh Lpr lFf" class="bg-brand">
+    <q-header elevated class="bgColor">
+      <q-toolbar style="height: 65px">
+        <q-btn
+          class="absolute-center"
+          style="color: #ffdd02"
+          flat
+          label="ยืนยัน"
+        />
+      </q-toolbar>
+    </q-header>
+  </q-layout>
+</template>
+
+<script>
+</script>
+
+<style>
+.bg-brand {
+  background: #010135;
+}
+.LoginButton {
+  font-size: 22px;
+  font-weight: bolder;
+  color: black;
+  background-color: white;
+}
+.userAndPass {
+  font-size: 22px;
+}
+</style>
