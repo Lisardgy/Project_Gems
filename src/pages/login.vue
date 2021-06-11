58693e48dee00a310ce47ce6f6c7b61dac08620a
Lisardgy <62231142+Lisardgy@users.noreply.github.com>
2021-06-11 07:57:53 +0700

    pushed

---
+++ src/pages/login.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    
+    <div class="cities">
+      
+    </div>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: null,
+    };
+  },
+};
+</script>
+
+<style scoped>
+  .q-layout{
+    background: #ffdd02;
+    
+  }
+
+  .cities{
+    background: url('../images/cities.png');
+    min-height: 812px;
+    background-position: bottom;
+    background-repeat: no-repeat;
+    background-size:contain;
+    
+    
+  }
+  
+
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,11 +2,15 @@
 const routes = [
   {
     path: '/',
-    component: () => import('src/pages/map.vue'),
+    component: () => import('src/layouts/MainLayout.vue'),
     children: [
       { path: 'ovs', name: "overviewStd", component: () => import('pages/overviewStandard.vue') },
-      { path: '/', name: "overviewCnd", component: () => import('pages/overviewCondo.vue') },
+      { path: '/ff', name: "overviewCnd", component: () => import('pages/overviewCondo.vue') },
       { path: '/ov4c', name: "overview4Cnd", component: () => import('pages/overviewforeCondo.vue') },
+      { path: '/map', name: "map", component: () => import('pages/overviewforeCondo.vue') },
+      { path: '/', name: "login", component: () => import('pages/login.vue') },
+      { path: '/editform', name: "editform", component: () => import('pages/overviewforeCondo.vue') },
+      { path: '/form', name: "form", component: () => import('pages/overviewforeCondo.vue') },
     ]
   },
 
