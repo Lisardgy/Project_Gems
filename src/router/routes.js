4cd314b2e1b74e05727020d5afed59f43776ac05
Lisardgy <62231142+Lisardgy@users.noreply.github.com>
2021-06-11 07:07:50 +0700

    another jayjay commid

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,11 +2,7 @@
 const routes = [
   {
     path: '/',
-<<<<<<< HEAD
     component: () => import('src/pages/map.vue'),
-=======
-    component: () => import('src/pages/editForm.vue'),
->>>>>>> edit_form_page
     children: [
       { path: 'ovs', name: "overviewStd", component: () => import('pages/overviewStandard.vue') },
       { path: '/', name: "overviewCnd", component: () => import('pages/overviewCondo.vue') },
