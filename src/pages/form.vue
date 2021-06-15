ecda1a498f22acb7a62a62d3a19687434fa34dda
Doublepor <doublepor.pp@gmail.com>
2021-06-16 05:17:52 +0700

    create vuex position and user_config

--- src/pages/form.vue
+++ src/pages/form.vue
@@ -948,6 +948,7 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
---
+++ src/store/data/user_config.js
@@ -0,0 +1,37 @@
+const state = {
+    uid: '',
+    email: '',
+    password: '',
+}
+
+const getters = {
+    getUserLogin: (state, getters, rootState) => {
+        return {
+            uid: state.uid,
+            email: state.email,
+            password: state.password,
+        };
+    }
+}
+
+const actions = {
+    setUserLogin: ({ commit, state }, payload) => {
+        commit('SET_USER', payload)
+    }
+}
+
+const mutations = {
+    SET_USER(state, payload) {
+        state.uid = payload.uid;
+        state.email = payload.email;
+        state.password = payload.password;
+    }
+}
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations,
+}
